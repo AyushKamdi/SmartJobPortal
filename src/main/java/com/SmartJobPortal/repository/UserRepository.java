ca824a8aaea117f53df949b9fdf653daa523e59b
Ayush Kamdi <88337078+AyushKamdi@users.noreply.github.com>
2026-05-06 10:54:34 +0530

    Update UserRepository.java

--- src/main/java/com/SmartJobPortal/repository/UserRepository.java
+++ src/main/java/com/SmartJobPortal/repository/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	// We can define custom search methods simply by naming them correctly
+
     Optional<User> findByUsername(String username);
     
     boolean existsByUsername(String username);
