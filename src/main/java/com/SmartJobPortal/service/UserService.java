de03a76749a326a87e8493cb67ba30e1b2c78354
Ayush Kamdi <88337078+AyushKamdi@users.noreply.github.com>
2026-05-06 10:55:01 +0530

    UserService.java

--- src/main/java/com/SmartJobPortal/service/UserService.java
+++ src/main/java/com/SmartJobPortal/service/UserService.java
@@ -4,10 +4,10 @@ import com.SmartJobPortal.model.User;
 
 public interface UserService {
 	
-	// This method will handle saving a new user to the database
+	
     User registerUser(User user);
     
-    // We'll need a way to check if an email or username is already taken
+ 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 
